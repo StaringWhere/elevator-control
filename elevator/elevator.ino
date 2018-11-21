d8d8244af989e092c227836961437b5f905c45e9
Staring Where <949047497@163.com>
2018-11-22 00:32:09 +0800

    4

--- elevator/elevator.ino
+++ elevator/elevator.ino
@@ -17,10 +17,11 @@ int height=0; //电梯高度/mm
 int drct=0; //运行方向，0为静止，1为向上，2为向下
 int vlct=0; //电梯速度/(mm/100ms)
 int i;
-char order[NUM_OF_ORDER]={'\0'}; //无序指令串
+char order[NUM_OF_ORDER]={0}; //无序指令串
 int target=0; //目标楼层/层,0代表无指令
-int door=0; //0关门，1开门
-int hold=0; //电梯是否无动作（包括开关门）
+int door=0; //电梯门的开合程度，0为关门，4为完全打开
+int checkdoor=0; //检查电梯门是否打开，0为关闭，1为打开
+int hold=0; //电梯是否无动作（包括开关门），0为有动作，1为无动作
 unsigned long stoptime=0;
 unsigned long starttime=0;
 
@@ -28,8 +29,8 @@ void setup() {
     Serial.begin(9600);
     pinMode(OPEN_PIN,INPUT);
     pinMode(CLOSE_PIN,INPUT);
-    attachInterrupt(0,open,RISING); //0对应2号引脚
-    attachInterrupt(1,close,RISING); //1对应3号引脚
+    attachInterrupt(digitalPinToInterrupt(2),open,RISING); 
+    attachInterrupt(digitalPinToInterrupt(3),close,RISING);
     lcd.begin(16, 2);
     while(Serial.available()<=0); //等待开始
     Serial.read(); //读掉's'
@@ -37,37 +38,53 @@ void setup() {
 }
 
 void loop(){
-    getorder(); //更新命令串
-    for(i=0;i<NUM_OF_ORDER;i++){ //显示指令串
-        lcd.setCursor(i,0);
-        lcd.write(order[i]+48);
+    if(checkdoor!=0){ //门未完全关闭执行的循环
+        getorder();
+        showorder();
+        if(order[height/3000*3]==1||order[height/3000*3+drct]==1){ //无方向/顺电梯方向指令触发开门
+            open();
+        }
+        delorder();  //尽量缩短getorder和delorder之间的距离，以增大遥控器信号有效概率
+        showdoor(); //放在主函数里以免lcdcursor错位
+        if(drct==0){ //静止方向的电梯从内部开门,根据第一个指令判断方向，先上后下
+            catchthefirst();
+            showdrct();
+        };
+        delay(100); //放在delorder后，使遥控器接收大几率落在delorder到getorder之间
+        return;
     }
+    getorder(); //更新指令串
+    showorder(); //显示指令串
     search(); //搜索指令，赋值给target，无指令则target=0
-    lcd.setCursor(0,1);
-    lcd.print(target);
+    showtarget();
     if(target==0){
         drct=0;
+        showdrct();
         //返回一层计时
+        detachInterrupt(digitalPinToInterrupt(2)); //防止中断之后出现立刻触发返回一层
+        detachInterrupt(digitalPinToInterrupt(3)); 
         if(hold==0){
             hold=1;
             starttime=millis();
             stoptime=starttime;
         }
         stoptime=millis();
-        if(stoptime-starttime>=60000)
-            Serial.print('b'); //发送返回一层指令
+        if(height!=0&&stoptime-starttime>=60000) //在一层不发出返回一层指令
+            Serial.print('b'); //发送返回一层指令    
+        attachInterrupt(digitalPinToInterrupt(2),open,RISING); 
+        attachInterrupt(digitalPinToInterrupt(3),close,RISING);
         return;
     }
-    hold=0;
+    hold=0; //退出无动作状态
     move(); //根据目标更新电梯状态(方向，高度，速度)
     if(height==(target-1)*3000){ //到达目标楼层
         inheritdrct(); //继承指令方向
         delorder(); //删除已完成指令
+        target=0;
         open(); //开门
     }
-    delay(50);
-    show(); 
-    delay(50); //错开接收时间
+    show(); //更新所有显示
+    delay(100);
 }
 
 void getorder(){ //更新指令串
@@ -79,7 +96,7 @@ void getorder(){ //更新指令串
 }
 
 void delorder(){ //整楼层消除命令信号
-    Serial.print(char((target-1)*3+drct)); //楼层数*3-1+运行方向
+    Serial.print(char(height/3000*3+drct)); //楼层数*3-1+运行方向
 }
 
 void search(){ //搜索指令，赋给target,无指令则target=0
@@ -168,22 +185,36 @@ void move(){ //根据指令更新电梯状态(方向，高度，速度)
 void close(){
     if(vlct!=0)
         return;
-    MsTimer2::stop(); //停止计时
-    MsTimer2::set(3000,STD);
+    MsTimer2::set(800,closing);
     MsTimer2::start();
 }
-void STD(){
-    door=0;
+void closing(){
+    if(door>0){
+        door--;
+    }
+    if(door==0){
+        checkdoor=0;
+        MsTimer2::stop();
+    }
 }
 
 void open(){
     if(vlct!=0)
         return;
-    door=1;
-    MsTimer2::stop(); //停止计时
-    MsTimer2::set(10000,close); //关门计时10s
+    checkdoor=1;
+    hold=0; //重置返回一层计时器
+    MsTimer2::set(800,opening); //逐渐开门
     MsTimer2::start(); //开始计时
-    while(door==1);
+}
+
+void opening(){
+    if(door<4){
+        door++;
+    }
+    if(door==4){
+        MsTimer2::set(8000,close); //关门倒计时
+        MsTimer2::start();
+    }
 }
 
 void back(){
@@ -191,21 +222,49 @@ void back(){
 }
 
 void show(){
+    showorder();
+    showheight();
+    showdrct();
+    showvlct();
+    showdoor();
+    showtarget();
+    //led();
+}
+void led(){ //更新led显示
+    Serial.print(char((height/3000)*3+drct+20)); //输出
+}
+void showorder(){ //更新指令串显示
+    lcd.setCursor(0,0);
+    for(i=0;i<NUM_OF_ORDER;i++)
+        lcd.write(order[i]+48);
+}
+void showheight(){ //更新高度显示
     lcd.setCursor(2,1);
     i=5-countdigit(height)-(height==0);
     while(i--){ //补全位数
         lcd.write(' ');
     }
     lcd.print(height);
+}
+void showdrct(){ //更新方向显示
     lcd.setCursor(8,1);
     lcd.print(drct);
+}
+void showvlct(){ //更新速度显示
     lcd.setCursor(10,1);
     i=3-countdigit(vlct)-(vlct==0);
     while(i--){
         lcd.write(' ');
     }
     lcd.print(vlct);
-    Serial.print(char((height/3000)*3+drct+20)); //输出
+}
+void showdoor(){ //更新门的状态显示
+    lcd.setCursor(14,1);
+    lcd.print(door);
+}
+void showtarget(){ //更新目标楼层显示
+    lcd.setCursor(0,1);
+    lcd.print(target);
 }
 
 int countdigit(int p){ //求整数位数
@@ -218,23 +277,44 @@ int countdigit(int p){ //求整数位数
 }
 
 void inheritdrct(){ //继承指令方向，若指令无方向，则继承之前电梯方向
+    int i=height/3000;
     if(drct<2){
-        if(order[(target-1)*3+1]==1){
+        if(order[i*3+1]==1){
             drct=1;
             return;
         }
-        if(order[(target-1)*3+2]==1){
+        if(order[i*3+2]==1){
             drct=2;
             return;
         }
     }
     else{
-        if(order[(target-1)*3+2]==1){
+        if(order[i*3+2]==1){
             return;
         }
-        if(order[(target-1)*3+1]==1){
+        if(order[i*3+1]==1){
             drct=1;
             return;
         }
+    }
+}
+
+void catchthefirst(){ //根据第一个命令改变方向
+    int i;
+    for(i=NUM_OF_ORDER-1;i>=0;i--){ //从高向低搜索
+        if(order[i]==1)
+            break;
+    }
+    if(i>=0){
+        if(i/3==height/3000){
+            if(order[i/3*3+1]==1)
+                drct=1;
+            else if(order[i/3*3+2]==1)
+                drct=2;
+        }
+        else if(i/3>height/3000)
+            drct=1;
+        else
+            drct=2;
     }
 }
--- test/millis/millis.ino
+++ test/millis/millis.ino
@@ -1,35 +1,40 @@
 #include<Arduino.h>
 #include<MsTimer2.h>
-//外部中断内部的delay和millis无效
+//外部中断内部的delay无效，millis静止
 //外部中断期间，外部millis静止
 //外部中断期间，外部delay静止
-//定时器中断内部的delay和millis无效
+//定时器中断内部的delay无效，millis静止
 //定时器中断期间，外部的delay静止
 //定时器的计时从每次定时函数执行完开始
 //定时中断函数中插入外部中断，函数结束后执行
+//定时器中断函数中插入定时器中断，函数结束后执行
 
 unsigned long time;
-
+int i=1;
 void setup(){
     Serial.begin(9600);
     MsTimer2::set(1000,go);
-    MsTimer2::start();
-    attachInterrupt(0,hi,RISING);
+    //MsTimer2::start();
+    attachInterrupt(digitalPinToInterrupt(2),hi,RISING);
+    Serial.println(i/3*3);
 }
 
 void loop(){
-    delay(5000);
-    Serial.println('a');
 }
 
 void go(){
     Serial.println('1');
+    MsTimer2::set(100,hi);
+    MsTimer2::start();
+    int i=1000;
+    while(i--)
+        Serial.println('3');
 }
 
 void hi(){
-    int i;
-    for(i=0;i<1000;i++){
-        Serial.print('w');
-    }
+    // int i;
+    // for(i=0;i<1000;i++){
+    //     Serial.print('w');
+    // }
     Serial.println('2');
 }
